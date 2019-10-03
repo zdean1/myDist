5d7a826f7a8c9a6306e42c67913d5224c158deab
Zach Dean <zdean3721@gmail.com>
2019-10-03 17:55:35 -0500

    article component added

--- Hw3/angular-reddit/src/app/app.component.ts
+++ Hw3/angular-reddit/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
-    console.log('Adding article title: ${title.value} and link: ${link.value}');
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     return false;
   }
 }
