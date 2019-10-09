66e372c36e6ac05481ae522a219ad7e03f9df510
Zach Dean <zdean3721@gmail.com>
2019-10-09 15:13:01 -0500

    flag button added for administrators

--- Hw3/angular-reddit/src/app/app.component.ts
+++ Hw3/angular-reddit/src/app/app.component.ts
@@ -11,14 +11,14 @@ export class AppComponent {
 
   constructor() {
     this.articles = [
-      new Article('Angular', 'http://angular.io', 3),
-      new Article('Fullstack', 'http://fullstack.io', 2),
-      new Article('Angular Homepage', 'http://angular.io', 1)
+      new Article('Angular', 'http://angular.io', "administrator", 3),
+      new Article('Fullstack', 'http://fullstack.io', "user", 2),
+      new Article('Angular Homepage', 'http://angular.io', "user", 1)
     ];
   }
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
-    this.articles.push(new Article(title.value, link.value, 0));
+    this.articles.push(new Article(title.value, link.value, '', 0));
     title.value = '';
     link.value = '';
     return false;
