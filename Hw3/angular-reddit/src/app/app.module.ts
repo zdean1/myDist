8beb7c825d63c37e72246c5d4afe8622e91dea96
Zach Dean <zdean3721@gmail.com>
2019-10-09 15:13:55 -0500

    flag article service added

--- Hw3/angular-reddit/src/app/app.module.ts
+++ Hw3/angular-reddit/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
+import { FlagArticleServiceService } from './flag-article-service.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { ArticleComponent } from './article/article.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [FlagArticleServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
